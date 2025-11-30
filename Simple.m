a22e959ba154eae706ce34228282f3992bca326d
Larissa de Ruiter <l.d.ruiter@student.tue.nl>
2025-11-30 20:37:16 +0100

    plot and CA adjustments

--- Simple.m
+++ Simple.m
@@ -26,7 +26,7 @@ Cyl.Bore                = 104*mm;
 Cyl.Stroke              = 85*mm;
 Cyl.CompressionRatio    = 21.5;
 Cyl.ConRod              = 136.5*mm;
-Cyl.TDCangle            = 180;
+Cyl.TDCangle            = 0;
 % -- Valve closing events can sometimes be seen in fast oscillations in the pressure signal (due
 % to the impact when the Valve hits its seat).
 CaIVO = -355;
@@ -37,7 +37,7 @@ CaSOI = -3.2;
 % Write a function [V] = CylinderVolume(Ca,Cyl) that will give you Volume
 % for the given Cyl geometry. If you can do that you can create pV-diagrams
 %% Load fdaq data (if txt file)
-FullName        = fullfile('Data','ExampleDataSet.txt');
+FullName        = fullfile('Data','20251125_0000001_imep_1.5_injection_18_fdaq.txt');
 dataIn          = table2array(readtable(FullName));
 [Nrows,Ncols]   = size(dataIn);                    % Determine size of array
 NdatapointsperCycle = 720/0.2;                     % Nrows is a multitude of NdatapointsperCycle
@@ -49,7 +49,8 @@ p_raw           = reshape(dataIn(:,2),[],Ncycles)*bara; % type 'help reshape' in
 p_avg = mean(p_raw, 2); % Calculate the average pressure for each crank angle
 
 %% Load sdaq data
-IntakeData = table2array(readtable(...));
+sDaq        = fullfile('Data','20251125_0000001_imep_1.5_injection_18_sdaq.txt');
+IntakeData  = table2array(readtable(sDaq));
 p_intake = mean(IntakeData(:,4))*bara; % average intake pressure in Pa
 
 %% Pegging
@@ -62,13 +63,13 @@ f1=figure(1);
 set(f1,'Position',[ 200 800 1200 400]);             % Just a size I like. Your choice
 pp = plot(Ca,p_avg_pegged/bara,'LineWidth',1);                 % Plots the whole matrix
 xlabel('Ca');ylabel('p [bar]');                     % Always add axis labels
-xlim([-360 360]);ylim([0 50]);                      % Matter of taste
+xlim([-360 360]);ylim([0 60]);                      % Matter of taste
 iselect = 10;                                    % Plot cycle 10 again in the same plot to emphasize it. Just to show how to access individual cycles.
 line(Ca(:,iselect),p_avg_pegged/bara,'LineWidth',2,'Color','r');
 YLIM = ylim;
 % Add some extras to the plot
-line([CaIVC CaIVC],YLIM,'LineWidth',1,'Color','b'); % Plot a vertical line at IVC. Just for reference not a particular reason.
-line([CaEVO CaEVO],YLIM,'LineWidth',1,'Color','r'); % Plot a vertical line at EVO. Just for reference not a particular reason.
+%line([CaIVC CaIVC],YLIM,'LineWidth',1,'Color','b'); % Plot a vertical line at IVC. Just for reference not a particular reason.
+%line([CaEVO CaEVO],YLIM,'LineWidth',1,'Color','r'); % Plot a vertical line at EVO. Just for reference not a particular reason.
 set(gca,'XTick',[-360:60:360],'XGrid','on','YGrid','on');        % I like specific axis labels. Matter of taste
 title('All cycles in one plot.')
 %% pV-diagram
@@ -78,13 +79,13 @@ set(f2,'Position',[ 200 400 600 800]);              % Just a size I like. Your c
 subplot(2,1,1)
 plot(V/dm^3,p_avg_pegged/bara);
 xlabel('V [dm^3]');ylabel('p [bar]');               % Always add axis labels
-xlim([0 0.8]);ylim([0.5 50]);                      % Matter of taste
+xlim([0 0.8]);ylim([0.5 60]);                      % Matter of taste
 set(gca,'XTick',[0:0.1:0.8],'XGrid','on','YGrid','on');        % I like specific axis labels. Matter of taste
 title({'pV-diagram'})
 subplot(2,1,2)
 loglog(V/dm^3,p_avg_pegged/bara);
 xlabel('V [dm^3]');ylabel('p [bar]');               % Always add axis labels
-xlim([0.02 0.8]);ylim([0 50]);                      % Matter of taste
+xlim([0.02 0.8]);ylim([0 60]);                      % Matter of taste
 set(gca,'XTick',[0.02 0.05 0.1 0.2 0.5 0.8],...
-    'YTick',[0.5 1 2 5 10 20 50],'XGrid','on','YGrid','on');        % I like specific axis labels. Matter of taste
+    'YTick',[0.5 1 2 5 10 20 60],'XGrid','on','YGrid','on');        % I like specific axis labels. Matter of taste
 title({'pV-diagram'})
